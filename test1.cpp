ec5bd363dfb534965f5a3b57fd3b3b5484b9980d
Hubert0916 <h237k77777@gmail.com>
2022-04-29 13:41:20 +0800

    0429

---
+++ test1.cpp
@@ -0,0 +1,57 @@
+//title採用動態記憶體配置
+#include <iostream>
+#include <cstring>
+using namespace std;
+class CWin 
+{
+    private:
+        char id,*title;
+    
+    public:
+        CWin(char i='D',const char *text="Default window"):id(i)
+        {
+            title=new char[50];
+            strcpy(title,text);
+        }
+        CWin operator=(CWin const& win)
+        {
+            id=win.id;
+            strcpy(this->title,win.title);
+            
+            return *this;
+        }
+        void show(void)
+        {
+            cout << "Window " << id << ": " << title << endl;
+        }
+        ~CWin()
+        {
+            delete [] title;
+        }
+        CWin(const CWin &win)
+        {
+            id=win.id;
+            strcpy(title,win.title);
+        }
+};
+
+int main(void)
+{
+    CWin win1('A',"Main window");
+    CWin win2('B',"Big window");
+    CWin win3;
+
+    win1.show();
+    win2.show();
+    win3.show();
+
+    win1=win2=win3;//win1.operator=(win2.operator=(win3))
+
+    cout << "after..." << endl;
+
+    win1.show();
+    win2.show();
+    win3.show();
+
+    return 0;
+}
